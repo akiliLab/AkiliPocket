68315be21a6ea5b0c849fcb87bfefd4b6bf46c46
MAKOSCAFEE <barnabasmakonda@gmail.com>
2019-05-15 22:32:57 +0300

    added bottom navigation items

--- app/src/main/java/design/akililab/akilipocket/MainActivity.kt
+++ app/src/main/java/design/akililab/akilipocket/MainActivity.kt
@@ -2,7 +2,13 @@ package design.akililab.akilipocket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+
+import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.onNavDestinationSelected
 import design.akililab.akilipocket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +16,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
+
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
+    
+
+
 }
