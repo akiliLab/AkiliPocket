2e989b87b29cc082b8dbcd4214f6307f1339fb5e
MAKOSCAFEE <barnabasmakonda@gmail.com>
2019-05-15 16:22:25 +0300

    added flagment for navigation

--- app/src/main/java/design/akililab/akilipocket/MainActivity.kt
+++ app/src/main/java/design/akililab/akilipocket/MainActivity.kt
@@ -2,11 +2,14 @@ package design.akililab.akilipocket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import design.akililab.akilipocket.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
