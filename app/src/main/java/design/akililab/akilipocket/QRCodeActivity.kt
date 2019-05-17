41bcdf3b4eda1e6f5a0a871444bde579295b8f08
MAKOSCAFEE <barnabasmakonda@gmail.com>
2019-05-17 17:09:15 +0300

    Added library and camera operations to be used for QR code detection

--- app/src/main/java/design/akililab/akilipocket/QRCodeActivity.kt
+++ app/src/main/java/design/akililab/akilipocket/QRCodeActivity.kt
@@ -2,13 +2,19 @@ package design.akililab.akilipocket
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import design.akililab.akilipocket.databinding.ActivityQrcodeBinding
 
 
 class QRCodeActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_qrcode)
+
+        @Suppress("UNUSED_VARIABLE")
+
+        val binding = DataBindingUtil.setContentView<ActivityQrcodeBinding>(this, R.layout.activity_qrcode)
 
     }
 
