8cf7258164c4cadd7b5d3642ead3ccc0f9b9bf2e
MAKOSCAFEE <barnabasmakonda@gmail.com>
2019-05-17 15:18:37 +0300

    added QR code activity

---
+++ app/src/main/java/design/akililab/akilipocket/QRCodeActivity.kt
@@ -0,0 +1,15 @@
+package design.akililab.akilipocket
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class QRCodeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrcode)
+
+    }
+
+}
