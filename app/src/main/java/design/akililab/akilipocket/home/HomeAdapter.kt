d024b6055c4ac3a49052523cda3ca7c924ae8ea0
MAKOSCAFEE <barnabasmakonda@gmail.com>
2019-05-26 12:42:12 +0300

    refactor adopter to separate concerns

--- app/src/main/java/design/akililab/akilipocket/home/HomeAdapter.kt
+++ app/src/main/java/design/akililab/akilipocket/home/HomeAdapter.kt
@@ -22,28 +22,65 @@ class HomeAdapter : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        val amount = Math.abs(item.amount.div(100))
-        holder.transactionAmount.text = amount.toString()
-        holder.transactionNote.text = item.notes
-        holder.transactionName.text = item.description.split("            ")[0]
-        // This should implement the required category type
-        holder.transactionIcon.setImageResource(R.drawable.googleg_standard_color_18)
+        holder.bind(item)
     }
 
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-
-        val view = layoutInflater.inflate(R.layout.list_item_transaction, parent, false)
-
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val transactionName: TextView = itemView.findViewById(R.id.transaction_name)
-        val transactionAmount: TextView = itemView.findViewById(R.id.transaction_amount)
-        val transactionNote: TextView = itemView.findViewById(R.id.transaction_note)
-        val transactionIcon: ImageView = itemView.findViewById(R.id.transaction_icon)
+        private val transactionName: TextView = itemView.findViewById(R.id.transaction_name)
+        private val transactionAmount: TextView = itemView.findViewById(R.id.transaction_amount)
+        private val transactionNote: TextView = itemView.findViewById(R.id.transaction_note)
+        private val transactionIcon: ImageView = itemView.findViewById(R.id.transaction_icon)
+
+
+        fun bind(
+            item: Transaction
+        ) {
+            val amount = Math.abs(item.amount.div(100))
+            transactionAmount.text = amount.toString()
+            transactionNote.text = item.notes
+            transactionName.text = item.description.split("            ")[0]
+            // This should implement the required category type
+            transactionIcon.setImageResource(
+                when (item.category) {
+
+                    EATING_OUT -> R.drawable.googleg_standard_color_18
+                    SHOPPING -> R.drawable.googleg_standard_color_18
+                    EXPENSE -> R.drawable.googleg_standard_color_18
+                    GENERAL -> R.drawable.googleg_standard_color_18
+                    TRANSPORT -> R.drawable.googleg_standard_color_18
+                    CASH -> R.drawable.googleg_standard_color_18
+                    BILLS -> R.drawable.googleg_standard_color_18
+                    HOLIDAY -> R.drawable.googleg_standard_color_18
+                    GROCERIES -> R.drawable.googleg_standard_color_18
+
+                    else -> R.drawable.googleg_standard_color_18
+                }
+            )
     }
+        companion object {
+            const val EATING_OUT = "eating_out"
+            const val SHOPPING = "shopping"
+            const val EXPENSE = "expense"
+            const val GENERAL = "general"
+            const val TRANSPORT = "transport"
+            const val CASH = "cash"
+            const val BILLS = "bills"
+            const val HOLIDAY = "holidays"
+            const val GROCERIES = "groceries"
+
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+
+                val view = layoutInflater.inflate(R.layout.list_item_transaction, parent, false)
+
+                return ViewHolder(view)
+            }
+        }
+    }
+
 }
